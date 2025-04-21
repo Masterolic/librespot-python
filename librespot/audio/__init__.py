7f515b19fcbfa0097421f2c9986608fc5cb94f72
Masterolic <rockingmallu8@gmail.com>
2025-04-21 20:14:54 +0530

    Update __init__.py

--- librespot/audio/__init__.py
+++ librespot/audio/__init__.py
@@ -818,7 +818,7 @@ class PlayableContentFeeder:
                 track_id_or_track)
             track = self.pick_alternative_if_necessary(original)
             if track is None:
-                raise RuntimeError("Cannot get alternative track")
+                raise ResourceNotAvailableError("Cannot get alternative track")
         else:
             track = track_id_or_track
         file = audio_quality_picker.get_file(track.file)
